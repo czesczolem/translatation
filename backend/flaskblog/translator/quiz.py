44e526e226b21d966812a347f0ec438163ea5c84
maciejt93 <maciej.tatarek93@gmail.com>
2020-05-09 19:28:29 +0200

    old changes, old mess

---
+++ backend/flaskblog/translator/quiz.py
@@ -0,0 +1,19 @@
+
+
+class Quiz:
+
+    def __init__(self):
+        self.questions = [{"en": "good", "pl": "dobrze"}, {"en": "bad", "pl": "źle"}]
+        self.user_vacab = []
+
+    def check_answer(self,):
+
+
+
+
+
+
+
+
+
+
--- backend/flaskblog/translator/routes.py
+++ backend/flaskblog/translator/routes.py
@@ -1,4 +1,4 @@
-from flask import (make_response, current_app, Blueprint, jsonify, request)
+from flask import (make_response, current_app, Blueprint, jsonify, request, render_template)
 from flask_login import current_user
 from flaskblog import db
 
@@ -15,3 +15,30 @@ def translate():
     res.headers['Access-Control-Allow-Origin'] = '*'
 
     return res
+
+@translator.route("/quiz")
+def quiz():
+    return render_template('quiz.html', title='Quiz')
+
+@translator.route("/quiz_check_answer", methods=['GET', 'POST'])
+def quiz_check_answer():
+    data = request.json
+    input_text = current_app.translator.get_input_text_from_request(data)
+    response_text = current_app.translator.translate(input_text)
+    response = {"text": response_text}
+    res = make_response(jsonify(response))
+    res.headers['Access-Control-Allow-Origin'] = '*'
+    # HERE NOW I SCHOULD CHANGE THIS FUNction and adjust it ot quiz schema, i can use just response no need for server send events imo
+    return res
+
+
+@translator.route("/sample_data")
+def sample_data():
+    response = [{
+        1:"nice",
+        2:"bad"
+    }]
+    data = {
+        "response": response
+    }
+    return jsonify(data)
